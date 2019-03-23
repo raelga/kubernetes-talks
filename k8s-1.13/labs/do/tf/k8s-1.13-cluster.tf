f9a614c646b767ca8077ad0abbde873af7173224
rael <me@rael.io>
2019-03-23 13:07:10 +0100

    Lowercase cluster names to avoid issues

--- k8s-1.13/labs/do/tf/k8s-1.13-cluster.tf
+++ k8s-1.13/labs/do/tf/k8s-1.13-cluster.tf
@@ -1,10 +1,10 @@
 resource "digitalocean_kubernetes_cluster" "k8s-cluster" {
-  name    = "BarcelonaCloud-k8s-1-13-tf"
+  name    = "barcelonacloud-k8s-1-13-tf"
   region  = "lon1"
   version = "1.13.4-do.0"
 
   node_pool {
-    name       = "BarcelonaCloud-k8s-1-13-np-tf"
+    name       = "barcelonacloud-k8s-1-13-np-tf"
     size       = "s-2vcpu-2gb"
     node_count = 1
   }
